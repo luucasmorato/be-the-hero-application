9223939752c2743cf308d01fccfc57e91a702a53
Lucas Morato <luucasmorato@gmail.com>
2020-08-02 20:53:01 -0300

    add layout

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,28 +1,30 @@
-const express = require('express');
-const OngController = require('./controllers/OngController');
-const IncidentsController = require('./controllers/IncidentController');
-const ProfileController = require('./controllers/ProfileController');
-const SessionController = require('./controllers/SessionController');
+const express = require("express");
+const {} = require("celebrate");
+
+const OngController = require("./controllers/OngController");
+const IncidentsController = require("./controllers/IncidentController");
+const ProfileController = require("./controllers/ProfileController");
+const SessionController = require("./controllers/SessionController");
 const routes = express.Router();
 
 //login
-routes.post('/sessions', SessionController.create);
+routes.post("/sessions", SessionController.create);
 
 //create ong
-routes.post('/ongs', OngController.create);
+routes.post("/ongs", OngController.create);
 
 //list ongs
-routes.get('/ongs', OngController.index);
+routes.get("/ongs", OngController.index);
 
 //create incident
-routes.post('/incidents', IncidentsController.create);
+routes.post("/incidents", IncidentsController.create);
 
 //list
-routes.get('/profile', ProfileController.index);
+routes.get("/profile", ProfileController.index);
 
-routes.get('/incidents', IncidentsController.index);
+routes.get("/incidents", IncidentsController.index);
 
 //delete incident
-routes.delete('/incidents/:id', IncidentsController.delete);
+routes.delete("/incidents/:id", IncidentsController.delete);
 
 module.exports = routes;
